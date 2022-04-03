ca57babe4f547e642b7e7c0cf839a61aa2c8d169
Rafal Wiezik <wiezikrafal@gmail.com>
2022-04-03 14:19:46 +0200

    First Day

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+
+        int throwDice;
+        int playerChoice;
+
+        do {
+            throwDice = rand.nextInt(6) + 1;
+            System.out.println("Kostka: " + throwDice);
+            playerChoice = rand.nextInt(6) + 1;
+            System.out.println("Gracz wybiera " + playerChoice);
+            if(throwDice != playerChoice)
+                System.out.println("Nie zgadłeś");
+            else
+                System.out.println("Zgadłeś");
+        }
+        while(throwDice != playerChoice);
+
+    }
+
 }
+
